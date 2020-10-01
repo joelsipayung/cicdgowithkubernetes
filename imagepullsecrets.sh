4ea866483fbb1dbb4527e9ad726b6a4ceb5b9b09
Joel Verdy Stevan Sipayung <jverdysipayung@gmail.com>
2020-10-01 20:12:56 +0700

    add sh secrets

---
+++ imagepullsecrets.sh
@@ -0,0 +1,1 @@
+kubectl create secret docker-registry regcred --docker-server=https://index.docker.io/v1/ --docker-username=joelsipayung --docker-password=<your-pword> --docker-email=jverdysipayung@gmail.com

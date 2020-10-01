65fe09669adbcaad8dc8226fdc645e9b275fa9b6
Joel Verdy Stevan Sipayung <jverdysipayung@gmail.com>
2020-10-01 18:24:13 +0700

    ubah conntent main go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World untuk yang terbaiklah!")
+		fmt.Fprintf(w, "Selamat datang di K DIgital !!!")
 	})
 
 	http.ListenAndServe(":80", nil)
